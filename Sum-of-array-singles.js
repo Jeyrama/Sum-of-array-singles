d1cd93c98c2f1f956f4da606c397301eb33cc246
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-06-24 19:12:59 -0700

    Update Sum-of-array-singles.js

--- Sum-of-array-singles.js
+++ Sum-of-array-singles.js
@@ -18,3 +18,19 @@ function repeats(arr){
 };
 
 // or
+
+function repeats(arr) {
+  let i;
+  let sum = 0;
+  arr.sort((a, b) => {
+    return a - b;
+  });
+  for (i = 0; i < arr.length; i += 2) {
+    if (arr[i] !== arr[i + 1]) {
+      sum += arr[i];
+      i--;
+    }
+  }
+
+  return sum;
+};
